573853a6a2c71df663f51286ea76e894df888145
yfoughal <yfoughali@coyote-group.com>
2019-01-31 11:48:15 +0100

    api key OK affichage classement buteurs 5 championnats OK

---
+++ app/src/main/java/com/yfoughali/foot/L1Activity.java
@@ -0,0 +1,103 @@
+package com.yfoughali.foot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class L1Activity extends AppCompatActivity {
+
+    private TextView text, text2;
+    private RequestQueue mQueue;
+    private Button buttonPL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l1);
+
+        text = findViewById(R.id.text);
+        text2 = findViewById(R.id.text2);
+        buttonPL = findViewById(R.id.buttonPL);
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonPL.setOnClickListener(PLlistener);
+
+        affichageClassementButeurs();
+    }
+
+    private void affichageClassementButeurs() {
+        String url = "https://api.football-data.org/v2/competitions/FL1/scorers";
+
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("scorers");
+
+
+                    for (int i = 0; i < 10; i++) {
+                        JSONObject buteurs = jsonArray.getJSONObject(i);
+                        JSONObject player = buteurs.getJSONObject("player");
+                        String namePlayer = player.getString("name");
+                        JSONObject team = buteurs.getJSONObject("team");
+                        String nameTeam = team.getString("name");
+                        int nbGoals = buteurs.getInt("numberOfGoals");
+                        text.append(namePlayer + "  " + nameTeam + "   " + nbGoals + "\n");
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        })
+
+        {
+            //Methode permettant de mettre la clé d'api dans le header
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                String apiKey = BuildConfig.ApiKey; // securisation de la clé d'API, ajoutée dans gradle.properties et build.gradle
+                headers.put("X-Auth-Token", apiKey);
+                return headers;
+
+            }
+        };
+
+        mQueue.add(request);
+    }
+
+    private View.OnClickListener PLlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent MainActivity = new Intent(L1Activity.this, MainActivity.class);
+            startActivity(MainActivity);
+
+        }
+    };
+}
--- app/src/main/java/com/yfoughali/foot/MainActivity.java
+++ app/src/main/java/com/yfoughali/foot/MainActivity.java
@@ -1,15 +1,17 @@
 package com.yfoughali.foot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -17,13 +19,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView text, text2;
     private  RequestQueue mQueue;
-    private Button button;
+    private Button buttonL1, buttonSA, buttonLIGA, buttonBL;
 
 
     @Override
@@ -34,17 +37,25 @@ public class MainActivity extends AppCompatActivity {
         text = findViewById(R.id.text);
         text2 = findViewById(R.id.text2);
         mQueue = Volley.newRequestQueue(this);
-        button = findViewById(R.id.calendrier);
+        buttonL1 = findViewById(R.id.buttonL1);
+        buttonSA = findViewById(R.id.buttonSA);
+        buttonLIGA = findViewById(R.id.buttonLIGA);
+        buttonBL = findViewById(R.id.buttonBL);
 
-        jsonTest();
+        buttonL1.setOnClickListener(L1listener);
+        buttonSA.setOnClickListener(SAlistener);
+        buttonLIGA.setOnClickListener(LIGAlistener);
+        buttonBL.setOnClickListener(BLlistener);
+
+        affichageClassementButeurs();
 
 
     }
 
-    private void jsonTest()
+    private void affichageClassementButeurs()
     {
-        String url = "https://api.myjson.com/bins/n63l4";
-        String apiKey = BuildConfig.ApiKey; // securisation de la clé d'API, ajoutée dans gradel.properties et build.gradle
+        String url = "https://api.football-data.org/v2/competitions/PL/scorers";
+
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -58,8 +69,10 @@ public class MainActivity extends AppCompatActivity {
                             JSONObject buteurs = jsonArray.getJSONObject(i);
                             JSONObject player = buteurs.getJSONObject("player");
                             String namePlayer = player.getString("name");
+                            JSONObject team = buteurs.getJSONObject("team");
+                            String nameTeam = team.getString("name");
                             int nbGoals = buteurs.getInt("numberOfGoals");
-                            text.append(namePlayer + "   " +nbGoals + "\n");
+                            text.append(namePlayer + "  "+ nameTeam +"   " +nbGoals + "\n");
                         }
 
 
@@ -76,10 +89,58 @@ public class MainActivity extends AppCompatActivity {
                 error.printStackTrace();
 
             }
-        });
+        })
+
+        {
+            //Methode permettant de mettre la clé d'api dans le header
+            public Map getHeaders() throws AuthFailureError
+            {
+                HashMap headers = new HashMap();
+                String apiKey = BuildConfig.ApiKey; // securisation de la clé d'API, ajoutée dans gradle.properties et build.gradle
+                headers.put("X-Auth-Token", apiKey);
+                return headers;
+
+            }
+        };
 
         mQueue.add(request);
     }
+
+    private View.OnClickListener L1listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent L1Activity = new Intent(MainActivity.this, L1Activity.class);
+            startActivity(L1Activity);
+
+        }
+    };
+
+    private View.OnClickListener SAlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent SAActivity = new Intent(MainActivity.this, SAActivity.class);
+            startActivity(SAActivity);
+
+        }
+    };
+
+    private View.OnClickListener LIGAlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent LIGAActivity = new Intent(MainActivity.this, LIGAActivity.class);
+            startActivity(LIGAActivity);
+
+        }
+    };
+
+    private View.OnClickListener BLlistener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent BLActivity = new Intent(MainActivity.this, BLActivity.class);
+            startActivity(BLActivity);
+
+        }
+    };
 
 }
 
